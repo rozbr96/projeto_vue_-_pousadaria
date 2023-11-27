4dd866b20930fe310965198a0241a8b0a164c5eb
Rosemilson <rozbr96@gmail.com>
2023-11-27 15:55:28 -0300

    Add basic table listing

---
+++ src/components/ListingItems.vue
@@ -0,0 +1,30 @@
+
+<script setup lang="ts">
+  import {
+    NSpace,
+    type DataTableColumn
+  } from 'naive-ui'
+
+  import { DataTable } from './tables'
+
+  defineProps<{
+    title: string
+    tableColumns: DataTableColumn[]
+    tableData: any[]
+  }>()
+</script>
+
+<template>
+  <NSpace
+    vertical
+    align='center'
+    :size="30"
+  >
+    <h1>{{ title }}</h1>
+
+    <DataTable
+      :columns="tableColumns"
+      :data="tableData"
+    />
+  </NSpace>
+</template>
---
+++ src/components/tables/DataTable.vue
@@ -0,0 +1,26 @@
+
+<script setup lang="ts">
+  import {
+    NDataTable
+  } from 'naive-ui'
+
+  const pageSizes = [5, 10, 20, 30, 50]
+  const pageSizesOptions = pageSizes.map(pageSize => {
+    return {
+      value: pageSize,
+      label: `${pageSize}/página`,
+      style: {
+        justifyContent: 'center'
+      }
+    }
+  })
+</script>
+
+<template>
+  <NDataTable
+    :pagination="{
+      pageSizes: pageSizesOptions,
+      showSizePicker: true
+    }"
+  />
+</template>
