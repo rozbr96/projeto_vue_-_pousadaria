322c11f24acce5390b4b3f4d1293af645755847e
Rosemilson <rozbr96@gmail.com>
2023-11-30 09:02:58 -0300

    Allow api endpoint to be set through environment variables

--- src/libs/api/index.ts
+++ src/libs/api/index.ts
@@ -1,9 +1,14 @@
 
+const HOST = import.meta.env.VITE_API_HOST || 'localhost'
+const PORT = import.meta.env.VITE_API_PORT || 3000
+const BASIC_ENDPOINT = import.meta.env.VITE_API_BASIC_ENDPOINT || '/api/v1'
+
+
 class API {
-  static HOST = 'http://localhost:3000/api/v1'
+  static FULL_HOST = `http://${HOST}:${PORT}/${BASIC_ENDPOINT}`
 
   static getFullEndpoint(endpoint: string) {
-    return `${API.HOST}${endpoint}`
+    return `${API.FULL_HOST}${endpoint}`
   }
 
   static get<T>(endpoint: string): Promise<T> {
